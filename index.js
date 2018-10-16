163262aac625d19c7e6905d6e22f959acf369e31
Evin J Manuel <evinjrocks169@gmail.com>
2018-10-17 01:45:17 +0530

    F:\Projects\InstaDoo

---
+++ index.js
@@ -0,0 +1,14 @@
+var express             = require("express"),
+    app                 = express(),
+    bodyParser          = require("body-parser"),
+    // mongoose            = require("mongoose");
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+// mongoose.connect("mongodb://localhost:27017/");
+
+// var campgroundSchema = new mongoose.Schema({
+//     name:String,
+//     image:String,
+//     description:String
+// });
