369de23f80eba87efe672af3a598b7aefbaae61c
Evin J Manuel <evinjrocks169@gmail.com>
2018-10-17 05:01:47 +0530

    Added header aand footer to partials, created post route and accepted the username and password

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express             = require("express"),
     app                 = express(),
-    bodyParser          = require("body-parser");
+    bodyParser          = require("body-parser"),
+    username                                    ,
+    mobile;
     // mongoose            = require("mongoose");
 
 app.set("view engine","ejs");
@@ -19,6 +21,15 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
+app.post("/login",function(req,res){
+    username=req.body.username;
+    mobile=req.body.mobile;
+    res.redirect("/user");
+});
+
+app.get("/user",function(req,res){
+    res.render("user",{username:username});
+});
 app.listen(3000,function(){
     console.log("It's on 3000");
 });
