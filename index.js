89b3127f617d702cb379b8860e00a140b98b38dc
Evin J Manuel <evinjrocks169@gmail.com>
2018-10-17 02:04:06 +0530

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var express             = require("express"),
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
 // mongoose.connect("mongodb://localhost:27017/");
 
 // var campgroundSchema = new mongoose.Schema({
@@ -12,3 +14,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 //     image:String,
 //     description:String
 // });
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.listen(3000,function(){
+    console.log("It's on 3000");
+});
