34a3b807929439e003ffcfb4e5aa111203877e3e
Evin J Manuel <evinjrocks169@gmail.com>
2018-10-17 01:50:40 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express             = require("express"),
     app                 = express(),
-    bodyParser          = require("body-parser"),
+    bodyParser          = require("body-parser");
     // mongoose            = require("mongoose");
 
 app.set("view engine","ejs");
