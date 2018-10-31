05d2e04f94a1cfc575342b753b068397e351d5af
Evin J Manuel <evinjrocks169@gmail.com>
2018-10-31 23:43:58 +0530

    Sign Up now accepts email and roomname and added bcrypt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express             = require("express"),
     app                 = express(),
+    bCrypt              = require('bcrypt'),
     bodyParser          = require("body-parser"),
     mongoose            = require("mongoose"),
     passport            = require("passport"),
@@ -23,9 +24,94 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(doctorUser.authenticate()));
 passport.serializeUser(doctorUser.serializeUser());
 passport.deserializeUser(doctorUser.deserializeUser());
+
+// passport login logic
+var isValidPassword = function(user, password){
+    return bCrypt.compareSync(password, user.password);
+  }
+
+passport.use('login',new LocalStrategy(
+    {    passReqToCallback : true
+    }, function(req, username, password, done) { 
+    // check in mongo if a user with username exists or not
+    doctorUser.findOne({ 'username' :  username }, 
+      function(err, user) {
+        // In case of any error, return using the done method
+        if (err)
+          return done(err);
+        // Username does not exist, log error & redirect back
+        if (!user){
+          console.log('User Not Found with username '+username);
+          return done(null, false, 
+                req.flash('message', 'User Not found.'));                 
+        }
+        // User exists but wrong password, log the error 
+        if (!isValidPassword(user, password)){
+          console.log('Invalid Password');
+          return done(null, false, 
+              req.flash('message', 'Invalid Password'));
+        }
+        // User and password both match, return user from 
+        // done method which will be treated like success
+        return done(null, user);
+      }
+    );}));
+
+//Passport sign up logic
+
+passport.use('signup', new LocalStrategy({
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+    findOrCreateUser = function(){
+      // find a user in Mongo with provided username
+      doctorUser.findOne({'username':username},function(err, user) {
+        // In case of any error return
+        if (err){
+          console.log('Error in SignUp: '+err);
+          return done(err);
+        }
+        // already exists
+        if (user) {
+          console.log('User already exists');
+          return done(null, false, 
+             req.flash('message','User Already Exists'));
+        } else {
+          // if there is no user with that email
+          // create the user
+          var newUser = new doctorUser();
+          // set the user's local credentials
+          newUser.username = username;
+          newUser.password = createHash(password);
+          newUser.email = req.param('email');
+          newUser.roomname = req.param('roomname');
+
+          // save the user
+          newUser.save(function(err) {
+            if (err){
+              console.log('Error in Saving user: '+err);  
+              throw err;  
+            }
+            console.log('User Registration succesful');    
+            return done(null, newUser);
+          });
+        }
+      });
+    };
+     
+    // Delay the execution of findOrCreateUser and execute 
+    // the method in the next tick of the event loop
+    process.nextTick(findOrCreateUser);
+  })
+);
+
+// Generates hash using bCrypt
+var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+   }
+
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
@@ -48,29 +134,20 @@ app.get("/user",function(req,res){
 app.get("/doctor/signup",function(req,res){
     res.render("doctor-signup");
 });
-app.post("/doctor/signup",function(req,res){
-    var details={roomname:req.body.roomname, email : req.body.email};
-    var Doctorname=new doctorUser({username: req.body.username}) ;
-    doctorUser.register(Doctorname,req.body.password,function(err,user){
-        if(err){
-            console.log(err)
-            return res.render("doctor-signup");
-        }
-        passport.authenticate("local")(req,res,function(){
-            user.push(details);
-            user.save();
-            console.log(user);
-            res.redirect("/doctor/dash");
-        });
-    });
-});
-app.get("/doctor/login",function(req,res){
+app.post("/doctor/signup",passport.authenticate('signup', {
+    successRedirect: "/doctor/dash",
+    failureRedirect:"/doctor/signup",
+    failureFlash : true 
+  }));
+  
+  
+  app.get("/doctor/login",function(req,res){
     res.render("doctor-login",{username:null});
 });
-app.post("/doctor/login",passport.authenticate("local", 
+app.post("/doctor/login",passport.authenticate("login", 
     {successRedirect:"/doctor/dash", 
     failureRedirect:"/doctor/login",
-    failureFlash: 'Invalid username or password.'})
+    failureFlash: true})
 , function(req,res){
 });
 app.get("/doctor/dash",isLoggedIn,function(req,res){
