5bf622cd26a332fd9cd1acb02ebf00b8715c5d8d
Evin J Manuel <evinjrocks169@gmail.com>
2018-11-25 02:43:31 +0530

    User page changes

--- routes/user.js
+++ routes/user.js
@@ -3,52 +3,53 @@ var express             = require("express"),
     doctorUser          = require("../models/Doctor"),
     flash               = require("connect-flash");
    
- var uname,mobile; 
 
-router.post("/user/login",function(req,res){
-    uname=req.body.username;
-    mobile=req.body.mnumber;
-    res.redirect("/user/profile");
-});
-router.get("/user/profile",isUserLogged, function(req,res){
-    console.log("user page");
-    res.render("./User/user");
+// get(id-->findbyid(middleware)-->loginpage)
+// post(:id-->/id (doctor,user,mobile)) 
+router.get("/:id",isDoctorCheck,function(req,res){
+    console.log("Login form");
+    console.log(req.session.doctor);
+   res.render("./User/Login",{doctorDetail:req.session.doctor});
+ 
 });
 
-router.get("/user/login",function(req,res){
-    console.log("User login page");
-    res.render("./User/Login");
-})
-router.get("/:id",isUserLogged,function(req,res){
-
+router.post("/:id",function(req,res){
+    req.session.username    = req.body.username;
+    req.session.mobile      = req.body.mnumber;
     doctorUser.findOne({userid:req.params.id},function(err,doctor){
         if(err){
-            
-            console.log('error in wait');
             console.log(err);
-            req.flash('error','Error while loading page');
-
+            req.flash('error','Error while logging in');
             res.redirect("/");
-        }
-        else if(doctor==null){
-            req.flash("error","No such doctor exist");
-            res.redirect("/user/login");
         }
         else{
             console.log("waiting room");
-            res.render("./User/wait",{doctordetail:doctor});
+            req.session.doctor = doctor;
+            console.log(req.session.doctor);
+            res.render("./User/wait",{doctordetail:req.session.doctor,username:req.session.username,mobile:req.session.mnumber});
         }
-    })
-});
-function isUserLogged(req,res,next){
-    if(uname!=null && mobile!=null){
-        console.log("user middleware");
-        return next();
-    }
-    else{
-        req.flash("error","Please Create a user acount first")
-        res.redirect("/user/login");
-    }
-}
+    });
 
+});
+
+function isDoctorCheck(req,res,next){
+    doctorUser.findOne({userid:req.params.id},function(err,doctor){
+        if(err){
+            console.log(err);
+            req.flash('error','Error while loading');
+            res.redirect("/");
+        }
+        else if(doctor==null)
+        {
+            req.flash('error','Error No such doctor exist');
+            res.redirect("/");  
+        }
+        else{
+            console.log("Doctor check middleware");
+            console.log(doctor);
+            req.session.doctor = doctor;
+            return next();
+        }
+    });
+}
 module.exports = router;
